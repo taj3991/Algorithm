2dc14d7ffb5060d54f1e3333a33c37c250d9b58c
taj <408751001@qq.com>
2016-12-09 00:57:07 +0800

    Go on!

--- src/jianzhiOffer/QuickSort.java
+++ src/jianzhiOffer/QuickSort.java
@@ -7,38 +7,74 @@ import java.util.Random;
  */
 public class QuickSort {
 
+    public static void main(String[] args) {
 
+        int[] arr = { 17,14};
+        quickSort(0, arr.length - 1, arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
 
-
-    static  void quickSort(int [] arr){
-        if (arr == null)return;
 
     }
 
-    static  void partition(int start,int end,int[] arr){
-        if (start == end || arr == null)return;
-        Random random = new Random();
-        int randomValue= random.nextInt(end-start);
-        int randomIndex  = start + randomValue;
-        int compareValue = arr[randomIndex];
-        swap(randomIndex,end,arr);
 
-        int left=0;
-        int right = end-1;
-        for(int i = start; left < right; i++){
-            if (arr[start] < compareValue){
-                arr[left++] = arr[i];
-            }else{
-                    while (arr[right--] >= compareValue);
-                    swap(i,right--,arr);
+    static void quickSort(int start, int end, int[] arr) {
+       if (start >= end || arr == null)return;
+
+        int index = partition2(start, end, arr);
+        if (index > start) {
+            quickSort(start, index - 1, arr);
+        }
+        if (index < end)
+            quickSort(index + 1, end, arr);
+
+    }
+
+    static int partition(int start, int end, int[] arr) {
+        int randomIndex = start;
+        swap(randomIndex, end, arr);
+
+        int left = start-1;
+        for (; start < end; start++) {
+            if (arr[start] < arr[end]) {
+                left++;
+
+                if (left != start)
+                    swap(left, start, arr);
             }
         }
-        swap(right,end,arr);
+        left++;
+        swap(left, end, arr);
+        return left;
     }
 
-    static  void swap(int i,int j,int [] arr){
+
+    static int partition2(int start, int end, int[] arr) {
+        int randomIndex = start;
+        swap(randomIndex, end, arr);
+
+        int left = start;
+        int right = end - 1;
+
+        for (; right >=  left ; start++) {
+            if (arr[start] < arr[end]) {
+                arr[left++] = arr[start];
+            } else {
+                while (arr[right] >= arr[end] && right > left) right--;
+                if ( right == left ) break;
+
+                swap(start, right--, arr);
+                left++;
+            }
+        }
+        swap(left, end, arr);
+        return left;
+    }
+
+    static void swap(int i, int j, int[] arr) {
         int tmp = arr[i];
-        arr [i] = arr[j];
-        arr [j] = tmp;
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
